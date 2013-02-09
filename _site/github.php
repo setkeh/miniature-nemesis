9ac9aa6a44e8a8f85d7cafa2b0cf0472087ead50
SETKEH <setkeh@gmail.com>
2013-02-09 21:41:12 +1100

    testing webhooks

--- _site/github.php
+++ _site/github.php
@@ -1,1 +1,2 @@
-<?php exec('cd .. && git pull', $out); print_r($out); ?>
+#<?php exec('cd .. && git pull', $out); print_r($out); ?>
+<?php ini_set('display_errors', '1'); exec('cd.. && git pull', $out); print_r($out); ?>
